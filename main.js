61ef78f43ed85d2207e6225e7b4ccdb43259d9e7
ashutoshpaliwal26 <112794799+ashutoshpaliwal26@users.noreply.github.com>
2024-02-26 18:57:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-
-
-
-const optData = async() =>{
+const val = document.querySelector('.search-box')
+const btn = document.querySelector('.btn')
+const optData = async(loc) =>{
     const api_key = '9255f2deae55c3075f41e5ac20eb1cba';
     const api_link= 'https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=';
-    let responce = await fetch(api_link + api_key + `&q=pune`);
+    let responce = await fetch(api_link + api_key +  `q=${loc}`);
     let data = await responce.json()
 
     const country = data.sys.country;
@@ -14,7 +13,13 @@ const optData = async() =>{
     const status = data.weather[0].main;
     document.querySelector('.weather').innerHTML = status;
     document.querySelector('.hi-low').innerHTML = Math.round(data.main.temp_min) + '/' + Math.round(data.main.temp_max);
-    document.querySelector('.date').innerHTML = new Date(day,'DD-MM-YYYY');
+    // document.querySelector('.date').innerHTML = new Date(day,'DD-MM-YYYY');
+    console.log(loc);
 }
+
+btn.addEventListener('click', ()=>{
+    optData(val.value);
+})
+
 
 optData();
